1da9bd92918b17297af64de1245f2b91093339cb
waieneiaw <waieneiaw@users.noreply.github.com>
2022-03-06 13:04:10 +0000

    deploy: 25fd0b2a209a46c1e0297d39f97946cd4beca9d6

---
+++ _next/static/I6hAeBAzrw1nCl92MLb-L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d69941380d67e919.css","static/chunks/pages/index-8a5854d695893fae.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
